8bd1b3ba65053209b0601fe07d617e17d1c2b91e
Alessandro Pignotti <a.pignotti@sssup.it>
2013-01-10 19:17:16 +0100

    Clean up and explain configuration bits

--- xonar_u1_setup.cpp
+++ xonar_u1_setup.cpp
@@ -13,58 +13,17 @@
  #define HID_REPORT_TYPE_FEATURE       0x03 
  
  
- #define CTRL_IN                LIBUSB_ENDPOINT_IN|LIBUSB_REQUEST_TYPE_CLASS|LIBUSB_RECIPIENT_INTERFACE 
- #define VENDOR_CTRL_IN         LIBUSB_ENDPOINT_IN|LIBUSB_REQUEST_TYPE_VENDOR|LIBUSB_RECIPIENT_INTERFACE 
  #define CTRL_OUT       LIBUSB_ENDPOINT_OUT|LIBUSB_REQUEST_TYPE_CLASS|LIBUSB_RECIPIENT_INTERFACE 
 
-void vendorRequestIn(libusb_device_handle* dev, int request, int value)
-{
-	unsigned char buf[20];
-	int ret=libusb_control_transfer(dev,LIBUSB_ENDPOINT_IN|LIBUSB_REQUEST_TYPE_VENDOR|LIBUSB_RECIPIENT_OTHER,request,value,0,buf,1,0);
-	printf("VI: R: %02x, V: %02x -> %02x\n",request,value,buf[0]);
-	assert(ret==1);
-}
-
-void vendorRequestOut(libusb_device_handle* dev, int request, int value, unsigned char v)
-{
-	unsigned char buf[20];
-	buf[0]=v;
-	int ret=libusb_control_transfer(dev,LIBUSB_ENDPOINT_OUT|LIBUSB_REQUEST_TYPE_VENDOR|LIBUSB_RECIPIENT_OTHER,request,value,0,buf,1,0);
-	assert(ret==1);
-}
-
-void classRequestOut0(libusb_device_handle* dev, int request, int value, int index)
-{
-	int ret=libusb_control_transfer(dev,LIBUSB_ENDPOINT_OUT|LIBUSB_REQUEST_TYPE_CLASS|LIBUSB_RECIPIENT_INTERFACE,request,value,index,NULL,0,0);
-	assert(ret==0);
-}
-
-void classRequestOut1(libusb_device_handle* dev, int request, int value, int index, unsigned char v)
-{
-	unsigned char buf[20];
-	buf[0]=v;
-	int ret=libusb_control_transfer(dev,LIBUSB_ENDPOINT_OUT|LIBUSB_REQUEST_TYPE_CLASS|LIBUSB_RECIPIENT_INTERFACE,request,value,index,buf,1,0);
-	assert(ret==1);
-}
-
-void classRequestOut2(libusb_device_handle* dev, int request, int value, int index, unsigned char v1, unsigned char v2)
-{
-	unsigned char buf[20];
-	buf[0]=v1;
-	buf[1]=v2;
-	int ret=libusb_control_transfer(dev,LIBUSB_ENDPOINT_OUT|LIBUSB_REQUEST_TYPE_CLASS|LIBUSB_RECIPIENT_INTERFACE,request,value,index,buf,2,0);
-	assert(ret==2);
-}
-
-void classRequestOut3(libusb_device_handle* dev, int request, int value, int index, unsigned char v1, unsigned char v2, unsigned char v3)
-{
-	unsigned char buf[20];
-	buf[0]=v1;
-	buf[1]=v2;
-	buf[2]=v3;
-	int ret=libusb_control_transfer(dev,LIBUSB_ENDPOINT_OUT|LIBUSB_REQUEST_TYPE_CLASS|LIBUSB_RECIPIENT_ENDPOINT,request,value,index,buf,3,0);
-	assert(ret==3);
-}
+//Setup of controls and leds
+//	bit 7 -> Enable interrupt to report the control status
+//	bit 6-5 -> Unknown/nothing
+//	bit 4-3 -> 11: Blue LED blinks, 00/01/10: Blue LED always on
+//	bit 2-1 -> 11: Red LED blinks always, 10: Red LED always off, 00/01: Red LED blinks on audio activity
+//	bit 0 -> Unknown/nothing
+#define ASUS_XONAR_U1_ENABLE_INTERRUPT (1<<7)
+#define ASUS_XONAR_U1_ENABLE_BLUE_LED_BLINKING (3<<3)
+#define ASUS_XONAR_U1_ENABLE_RED_LED_BLINKING (3<<1)
 
 int main()
 {
@@ -88,11 +47,12 @@ int main()
 	//The first report initializes the blinking
 	//Byte 0 is a command
 	//Byte 1 us unknown
-	//Byte 2 is a bitfield use to setup the control and leds
-	//	-> 0x80: Enable interrupt to report the control status
-	//	-> 0x18: Enable the blinking patterns
-	//	-> TODO: investigate lower bits
-	unsigned char setupReport[] = {0x02,0x01,0x18,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
+	//Byte 2 is a bitfield use to setup the controls and leds
+	unsigned char setupReport[] = {0x02,0x01,
+		ASUS_XONAR_U1_ENABLE_INTERRUPT|
+		ASUS_XONAR_U1_ENABLE_BLUE_LED_BLINKING|
+		ASUS_XONAR_U1_ENABLE_RED_LED_BLINKING
+			,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
 	ret=libusb_control_transfer(dev,LIBUSB_ENDPOINT_OUT|LIBUSB_REQUEST_TYPE_CLASS|LIBUSB_RECIPIENT_INTERFACE,0x9,0x200,3,
 			setupReport,16,0);
 	assert(ret==16);
@@ -101,7 +61,7 @@ int main()
 	//Byte 1 is unknown
 	//Byte 2 is time spent off
 	//Byte 3 is total duty cycle time (TODO: understand unit of time)
-	unsigned char reportOut5[] = {0x13,0x02,0x89,0x89,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
+	unsigned char reportOut5[] = {0x13,0x02,0x45,0x89,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
 	ret=libusb_control_transfer(dev,LIBUSB_ENDPOINT_OUT|LIBUSB_REQUEST_TYPE_CLASS|LIBUSB_RECIPIENT_INTERFACE,0x9,0x200,3,
 			reportOut5,16,0);
 	assert(ret==16);
