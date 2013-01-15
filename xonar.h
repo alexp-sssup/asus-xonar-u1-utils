10512ecc664675757241faf1315534b7ffb2bc9b
Alessandro Pignotti <a.pignotti@sssup.it>
2013-01-15 16:47:36 +0100

    Add license data

--- xonar.h
+++ xonar.h
@@ -1,3 +1,25 @@
+/*
+ * Xonard
+ *
+ * Copyright 2012-2013 Alessandro Pignotti
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef XONAR_H
+#define XONAR_H
+
 //Setup of controls and leds
 //	bit 7 -> Enable interrupt to report the control status
 //	bit 6-5 -> Unknown/nothing
@@ -11,3 +33,5 @@
 
 #define ASUS_XONAR_U1_BLUE_LED 0x13
 #define ASUS_XONAR_U1_RED_LED 0x11
+
+#endif
